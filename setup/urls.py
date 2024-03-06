2271db707a9fd85e6b52a0527a66278b56b51a9a
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-03-06 15:06:51 -0300

    fix add message

--- setup/urls.py
+++ setup/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
     path('', include('usuarios.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
